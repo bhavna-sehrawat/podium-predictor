f82bb9991a1bc00b7b743346bd7cf83b093a7618
Bhavna <sehrawatbhavna001@gmail.com>
2025-11-12 16:00:18 +0530

    feat: Define Prediction schema and model

---
+++ backend/model/prediction.model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const predictionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    race: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Race',
+      required: true,
+    },
+    polePosition: {
+      type: String,
+      required: [true, 'Pole position prediction is required'],
+      trim: true
+    },
+    podium: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (arr) {
+          return Array.isArray(arr) && arr.length === 3;
+        },
+        message: 'Podium must contain exactly 3 drivers.',
+      },
+    },
+    fastestLap: {
+      type: String,
+      required: [true, 'Fastest lap prediction is required'],
+      trim: true,
+    },
+    score: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+predictionSchema.index({ user: 1, race: 1 }, { unique: true });
+
+const Prediction = mongoose.model('Prediction', predictionSchema);
+
+export default Prediction;
